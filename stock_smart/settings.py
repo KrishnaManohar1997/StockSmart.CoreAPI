06fcd55fcd0aee5145b05a6652ba6dd62d5020c9
Krishna Manohar <krishna.m@cawstudios.com>
2021-05-24 23:15:07 +0530

    Token based auth

--- stock_smart/settings.py
+++ stock_smart/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "stock",
     "user",
     "rest_framework",
+    # Token Authentication
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,8 @@ STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
