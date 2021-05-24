996093f916db5679d7c5e7902ee2a989f4c9bcf3
Krishna Manohar <krishna.m@cawstudios.com>
2021-05-24 16:51:21 +0530

    Add user model changes with migrations

--- user/models/user_model.py
+++ user/models/user_model.py
@@ -4,9 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     objects = UserManager()
+    first_name = models.CharField(max_length=256, blank=False, null=False)
+    last_name = models.CharField(max_length=256, blank=False, null=False)
+    username = models.CharField(max_length=256, blank=True, null=True)
+    phone = models.CharField(max_length=16, unique=True, blank=True, null=True)
+    country_code = models.CharField(max_length=4, default="+91")
     email = models.EmailField(blank=False, null=False, unique=True)
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    REQUIRED_FIELDS = ["username", "first_name", "last_name", "phone"]
 
     def __str__(self):
         return self.email
